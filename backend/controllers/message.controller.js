116bc066f9ee810fea2b3723be3d2a33b4909b82
vishal yadav <vishal7738639800@gmail.com>
2024-04-18 17:14:55 +0530

    backend completed without socket.io

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,56 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    // Get All The Conversation Between Sender And Receiver
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    // If !Conversation, That Means This Is Their First Conversation
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = new Message({ senderId, receiverId, message });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+    // await conversation.save();
+    // await newMessage.save();
+
+    // This will run in parallel
+    await Promise.all([conversation.save(), newMessage.save()]);
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in message controller send message", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChat } = req.params;
+    const senderId = req.user._id;
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChat] },
+    }).populate("messages");
+
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+    const messages = conversation.messages;
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in message controller get messages", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+
+// File Import
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+
+// Variable
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Config
+dotenv.config();
+
+// Middleware
+// User Login, Logout, Signup Routes
+app.use(express.json()); //to parse the incoming req with JSON payloads
+app.use(cookieParser()); //to parse the cookie
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  // root route
+  res.send("Hello World!!");
+});
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is up and running on PORT : ${PORT}`);
+});
