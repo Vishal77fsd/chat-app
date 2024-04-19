d0b789d9c7ee48b768fa51f9b8a2a6cfb7fc0bbc
vishal yadav <vishal7738639800@gmail.com>
2024-04-19 19:55:48 +0530

    UI Design

---
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import Conversations from "./Conversations";
+import SearchInput from "./SearchInput";
+import LogoutButton from "./LogoutButton";
+const Sidebar = () => {
+  return (
+    <div>
+      <SearchInput />
+      <div className="divider px-3"></div>
+      <Conversations />
+      <LogoutButton />
+    </div>
+  );
+};
+
+export default Sidebar;
